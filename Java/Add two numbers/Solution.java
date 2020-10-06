ab25ca21d520ae019c83fe0750772b0296c8f1ac
Tanzilur-Rahman <71892355+Tanzilur-Rahman@users.noreply.github.com>
2020-10-06 14:51:22 +0600

    Add files via upload

---
+++ Java/Add two numbers/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter 1st nunber:");
+        int num1=input.nextInt();
+        System.out.println("Enter 1st nunber:");
+        int num2=input.nextInt();
+        int sum=num1+num2;
+        System.out.println("Sum="+sum);
+      
+    }
+    
+}
---
+++ Java/Multiplication table/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ 
+public class Solution {
+ 
+ public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+   
+  System.out.print("Input a number: ");
+  int num1 = in.nextInt();
+  for (int i=1; i<= 10; i++){
+   
+   int result=num1*i;
+   System.out.println(num1+"*"+i+"="+result);
+  }
+ }
+}
