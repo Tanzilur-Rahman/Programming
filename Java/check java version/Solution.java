21bbb3f0cde86113c857692a330f7f779a846f66
Tanzilur-Rahman <rahman15-6017@diu.edu.bd>
2020-10-11 15:00:33 +0600

    Add files via upload

---
+++ Java/check java version/Solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+ 
+ public static void main(String[] args) {
+   System.out.println("Java Version: "+System.getProperty("java.version"));
+   System.out.println("Java Runtime Version: "+System.getProperty("java.runtime.version"));
+ }
+}
